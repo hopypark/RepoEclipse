74dcfbb19738a6854eb826a0513c263592ec0250
hopypark <hopypark@gmail.com>
2023-01-12 20:57:54 +0900

    add movie name

--- HelloJava/src/HelloMain.java
+++ HelloJava/src/HelloMain.java
@@ -4,6 +4,8 @@ public class HelloMain {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
 		System.out.println("Welcome!!");
+		
+		System.out.println("Lost Symbol");
 	}
 
 }
